9f014be2eb7720a09a3dae9da7124e6981d6f6b7
vgaudard <victor.gaudard@gmail.com>
2016-03-02 23:23:13 +0100

    Youtube scripts executed on pushState

--- youtube_cleaner.js
+++ youtube_cleaner.js
@@ -10,22 +10,32 @@
 /* jshint -W097 */
 'use strict';
 
-var classesToDelete = ["addto-watch-later-button", "yt-uix-menu", "notifications-container"];
-var idsToDelete = ["trending-guide-item", "VLWL-guide-item", "create_channel-guide-item", "gh-navitem-trending"];
+function cleanYoutube() {
+    var classesToDelete = ["addto-watch-later-button", "yt-uix-menu", "notifications-container"];
+    var idsToDelete = ["trending-guide-item", "VLWL-guide-item", "create_channel-guide-item", "gh-navitem-trending"];
 
-var item;
+    var item;
 
-for (var i = 0; i < classesToDelete.length; i++) {
-    while (item = document.getElementsByClassName(classesToDelete[i])[0]) {
-        item.remove();
+    for (var i = 0; i < classesToDelete.length; i++) {
+        while (item = document.getElementsByClassName(classesToDelete[i])[0]) {
+            item.remove();
+        }
     }
+
+    for (var i = 0; i < idsToDelete.length; i++) {
+        document.getElementById(idsToDelete[i]).remove();
+    }
+
+
+    document.querySelector("#guide-container > div > ul > li:nth-child(2)").remove();
 }
 
-for (var i = 0; i < idsToDelete.length; i++) {
-    document.getElementById(idsToDelete[i]).remove();
-}
 
 
-document.querySelector("#guide-container > div > ul > li:nth-child(2)").remove();
-
-
+// Watch for page change
+var target = document.querySelector('#content');
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(cleanYoutube);    
+});
+var config = { attributes: false, childList: true, characterData: false };
+observer.observe(target, config);
--- youtube_rel.js
+++ youtube_rel.js
@@ -7,6 +7,18 @@
 // @run-at       document-end
 // ==/UserScript==
 /* jshint -W097 */
-'use strict'; 
+'use strict';
 
-document.querySelectorAll(".autoplay-bar .content-link")[0].setAttribute("rel", "next");
+function addYoutubeRel() {
+    document.querySelectorAll(".autoplay-bar .content-link")[0].setAttribute("rel", "next");
+}
+
+addYoutubeRel();
+
+// Watch for page change
+var target = document.querySelector('#content');
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(addYoutubeRel);
+});
+var config = { attributes: false, childList: true, characterData: false };
+observer.observe(target, config);
